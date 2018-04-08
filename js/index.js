955012017328f862054d57cd1865dc207f16f68d
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-04-08 13:49:18 -0400

    Updates to Library API project
    Added a new partial, added more content to the Title-Font mixin, completely re-worked the displayPage function in JavaScript to appropriately display book information, updated a BUNCH of SCSS to either A) cut out unneccessary/misplaced code and B) relocate certain code chunks to their appropriate spots. Biggest blunder I found was that majority of the code I wanted for my <li> elements was under <ul>, making my whole navigation the size of a button, and a very small button at that.

--- js/index.js
+++ js/index.js
@@ -5,48 +5,47 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#search");
 const results = document.querySelector("#results");
-const content = '';
+let content = '';
 
 form.addEventListener("submit", e => {
 	e.preventDefault();
-	value = input.value;
-	console.log(value);
+// Code line for outputting written user value to console
+//	  console.log(value);
 
 	// Create url-path for API
 	const query = input.value;
 	const url = `http://openlibrary.org/search.json?q=${query}`;
 
-// TESTING URL
+// DEBUGGING URL
 //const url = 'http://openlibrary.org/search.json?q=Iron-Man';
 
 // FETCH base + query
 fetch(url)
 	.then(data => data.json())
-	.then(data => console.log(data))
+// Code line used for testing in console
+//	  .then(data => console.log(data))
 	.then(data => displayPage(data))
 	.catch(error => console.log('Error Report:', error));
 })
 
+
 // Build results
 function displayPage(data) {
-
-	for(var i = 0; i < 101; i++) {
-		const { book } = data.docs[i];
-		const id = book.cover_edition_key;
+	data.docs.forEach(book =>{
 		content +=
-		`<article> 
-		<h1 id="title">${book.title[0]}</h1>
-		<h1 id="author">${book.author_name[0]}</h1>
-		<h2 id="pubYear">${book.first_publish_year[0]}</h2>
-		<h2 id="languages">${book.language[0]}</h2>
-		<h2 id="subject">${book.subject[0]}</h2>
-		<h2 id="publisher">${book.publisher[0]}</h2>
+			`<article> 
+			<h1>Book Title: ${book.title}</h1>
+			<h1>Author: ${book.author_name}</h1>
+			<h2>Year Published: ${book.first_publish_year}</h2>
+			<h2>Language(s): ${book.language}</h2>
+			</article>`;
+		});
+	// Additional information with pre-filled in array names.
+//			<h2>Gengre(s): ${book.subject}</h2>
+//			<h2>Characters: ${book.person}</h2>
+//			<h2>Locations: ${book.place}</h2>
 
-		<h2 id="cast">${book.person}</h2>
-		<h2 id="locations">${book.place}</h2>
-		</article>`;
-	};
 	// Populate the DOM
 	results.innerHTML = content;
 
-
+};
