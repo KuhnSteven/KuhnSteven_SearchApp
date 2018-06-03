654c0f1dc3c240170e6cc47341d51e990bb9610d
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-06-03 15:37:06 -0400

    Initial updates
    Started by working on textarea size, intending on adding a logo and relocating the "Book Search Engine" text

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 
 gulp.task("css", () => {
-      return gulp
-            .src("./scss/Styles.scss")
-            .pipe(sass().on("error", sass.logError))
-            .pipe(gulp.dest("./css"))
+  return gulp
+    .src("./scss/styles.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
 });
 
 gulp.task("watch", () => {
-      gulp.watch("./scss/*.scss", ["css"])
+  gulp.watch("./scss/*.scss", ["css"]);
 });
